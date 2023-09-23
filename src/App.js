50e52a9e415f386f937e1852f92fcea9861ddc17
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-09-23 11:27:55 +0200

    Create an advice component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 export default function App() {
     return (
-        <main>
-            <p>TEST</p>
+        <main className="main">
+            <h1 className="advice-number">
+                Advice #117
+            </h1>
+            <p className="advice-text">
+                "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+            </p>
+            <img className="divider" src="/images/pattern-divider-mobile.svg" alt="divider" />
+            <button className="button">
+                <img src="/images/icon-dice.svg" alt="dice" />
+            </button>
         </main>
     );
 }
