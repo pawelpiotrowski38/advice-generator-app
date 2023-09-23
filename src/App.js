dbf82c1327804b823dbaddf52fdd84de5f3444d3
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-09-23 15:53:03 +0200

    Add functionality of fetching a random advice with loading animation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
 
 export default function App() {
+    const [adviceNumber, setAdviceNumber] = useState(0);
+    const [adviceText, setAdviceText] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    const getAdvice = async function(id) {
+        try {
+            setIsLoading(true);
+            setError("");
+            const response = await fetch(`https://api.adviceslip.com/advice${id ? `/${id}` : ''}`,  { cache: "no-cache" });
+            if (!response.ok) {
+                throw new Error("Something went wrong. Please try again later.");
+            }
+
+            const data = await response.json();
+            if (data.slip === undefined) {
+                throw new Error("Advice not found. Please try again.");
+            }
+
+            setAdviceNumber(data.slip.id);
+            setAdviceText(data.slip.advice);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getAdvice(71);
+    }, [])
+
     return (
         <main className="main">
-            <h1 className="advice-number">
-                ADVICE #117
-            </h1>
-            <p className="advice-text">
-                “It is easy to sit up and take notice, what’s difficult is getting up and taking action.”
-            </p>
-            <picture>
-                <source srcSet="/images/pattern-divider-desktop.svg" media="(min-width: 33.75em)" />
-                <img className="divider" src="/images/pattern-divider-mobile.svg"  alt="divider" />
-            </picture>
-            {/* <img className="divider" src="/images/pattern-divider-mobile.svg" alt="divider" /> */}
-            <button className="button">
-                <img src="/images/icon-dice.svg" alt="dice" />
-            </button>
+            <div className={`advice-container ${isLoading ? 'advice-container--hidden' : ''}`}>
+                {!error ? (
+                    <>
+                        <h1 className="advice-number">
+                            {`ADVICE #${adviceNumber}`}
+                        </h1>
+                        <p className="advice-text">
+                            {!error ? `“${adviceText}”` : error}
+                        </p>
+                    </>
+                ) : (
+                    <p className="advice-text">
+                        {error}
+                    </p>
+                )}
+                <picture>
+                    <source srcSet="/images/pattern-divider-desktop.svg" media="(min-width: 33.75em)" />
+                    <img className="divider" src="/images/pattern-divider-mobile.svg"  alt="divider" />
+                </picture>
+                <button onClick={() => getAdvice(0)} className="button">
+                    <img src="/images/icon-dice.svg" alt="dice" />
+                </button> 
+            </div>
+            <div className={`loading-container ${isLoading ? 'loading-container--visible' : ''}`}>
+                {/* <p>Loading...</p> */}
+                <div className="loading-spinner"></div>
+            </div>
+         
         </main>
     );
 }
