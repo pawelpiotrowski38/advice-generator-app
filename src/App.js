238df075d22567c41866995c58c0f720bc3bbeb8
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-09-23 11:52:34 +0200

    Add styles to the mobile version of the component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import './styles.css';
+
 export default function App() {
     return (
         <main className="main">
             <h1 className="advice-number">
-                Advice #117
+                ADVICE #117
             </h1>
             <p className="advice-text">
-                "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+                “It is easy to sit up and take notice, what's difficult is getting up and taking action.”
             </p>
             <img className="divider" src="/images/pattern-divider-mobile.svg" alt="divider" />
             <button className="button">
