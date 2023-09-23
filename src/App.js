84a17e7d4e6afab4cbaa84fccfa94017ababd2e9
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-09-23 13:16:22 +0200

    Add styles for the desktop version of the component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ export default function App() {
                 ADVICE #117
             </h1>
             <p className="advice-text">
-                “It is easy to sit up and take notice, what's difficult is getting up and taking action.”
+                “It is easy to sit up and take notice, what’s difficult is getting up and taking action.”
             </p>
-            <img className="divider" src="/images/pattern-divider-mobile.svg" alt="divider" />
+            <picture>
+                <source srcSet="/images/pattern-divider-desktop.svg" media="(min-width: 33.75em)" />
+                <img className="divider" src="/images/pattern-divider-mobile.svg"  alt="divider" />
+            </picture>
+            {/* <img className="divider" src="/images/pattern-divider-mobile.svg" alt="divider" /> */}
             <button className="button">
                 <img src="/images/icon-dice.svg" alt="dice" />
             </button>
